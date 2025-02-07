f73d5843fe5b4b5bce9a8a2b2d5f8b089427d710
Jen Ova <legit.quidd60@gmail/com>
2025-02-07 15:58:57 -0800

    new program lol

---
+++ MySpecialApp/MySpecialApp/Program.cs
@@ -0,0 +1,103 @@
+﻿using System; // Uses system
+using System.Runtime.InteropServices;
+using System.Buffers.Binary;
+using System.Buffers.Text;
+using System.CodeDom.Compiler;
+using System.Collections.Concurrent;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Configuration.Assemblies;
+using System.Data.Common;
+using System.Data.SqlTypes; // The more you call the less likely you'll have errors!
+using System.Runtime.InteropServices;
+
+namespace MySpecialApp // Program
+{
+    class a // This is my code
+    {
+        static long thirdLeg = 1;
+        public string strung = "";
+        public                                                                                                                                                                                                                                                                                                                                                                                                                                                  string stringing = "";
+        class Inception<T> where T : Inception<T>
+{
+    public T InvokeTheCauseOfACenturyCOMMASomethingTrulyImportantToThisWorldCOMMATheClassOfALifeTimeAndATrulyBreathTakingAndImportantPERIOD(T input)
+    {
+        return input;
+    }
+}
+
+        public void NotMain(string[] args) // This uses C# code to perform commands
+        {
+            bool KeepRunning = true;
+            while(KeepRunning)
+            {
+                long a = 1; // I don't know how this was implemented
+                while(KeepRunning)
+                {
+                    a = 1; // Make sure to allocate memory, this is a C language after all
+                    a = 2;
+                    a = 1;
+                    a = 2;
+                    a = 1;
+                            IntPtr ptr = Marshal.AllocHGlobal(128);
+        Marshal.Copy(new byte[] { 72, 101, 108, 108, 111 }, 0, ptr, 5);
+        Console.WriteLine(Marshal.PtrToStringAnsi(ptr));
+        Marshal.FreeHGlobal(ptr);
+                    bool giveTheUserAVirus = true;
+                    string HELLO = "Hello";
+                    string WORLD = "world!";
+                    if(WORLD != HELLO)
+                    {
+                        string WORLD2 = HELLO;
+                        if(HELLO != WORLD)
+                        {
+                            var list = new List<int>(1000);
+                            string HELLO2 = WORLD;
+                            if(HELLO2 != WORLD2)
+                            {
+                                long HELLO3 = a;
+                                long b = HELLO3;
+                                long c;
+                                if(WORLD == WORLD2)
+                                {
+                                    string giveTheUserASecondVirus = "Randomware.exe"; // TODO: make the randomware lottery based, so users will gamble to rightfully earn their data back.
+                                }
+                            }
+                        }
+                    }
+                    while(KeepRunning)
+                    {
+                        a = 2 + 2 - 3;if (KeepRunning){a = 1;Console.Beep();Console.WriteLine(HELLO, WORLD);if(1+1 == 2){Console.WriteLine("1 + 1 equals two");}}if(1+1 != 2){Console.WriteLine("1 + 1 equals", 1 * 32892332452^7);}}}}}
+
+        public void HasStringBeenStrung() // This needs no explanation
+        {
+            if(stringing == strung)
+            {
+                Console.WriteLine("String has been strung!");
+                string ℱ = "ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜ℱ𝑜𝑜";
+            }
+        }
+        class b // This helps with the thing
+        {
+            void WeholdthesetruthstobeselfevidentthatallmenarecreatedequalthattheyareendowedbytheirCreatorwithcertainunalienableRightsthatamongtheseareLifeLibertyandthepursuitofHappiness()
+            {
+                long a = 1222222222222222222; // Very long 12
+                dynamic b = 5;
+b = "c";
+b = new { Name = "d" };
+Console.WriteLine(b.Name);
+
+                WeholdthesetruthstobeselfevidentthatallmenarecreatedequalthattheyareendowedbytheirCreatorwithcertainunalienableRightsthatamongtheseareLifeLibertyandthepursuitofHappiness();
+            }
+            class c // You always need a class C when you have a class b
+            {
+                string classclassclassguhhhhhhhglassclass = "7";
+            }
+        }
+    }
+}
