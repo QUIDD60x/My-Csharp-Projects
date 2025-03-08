3f7ba07a1edac2a12f3ceb4d950c50ca2bfdee0e
Jen Ova <legit.quidd60@gmail/com>
2025-03-07 17:35:32 -0800

    Added a fake virus program, along with dungeon game updates

--- DungeonGame/DungeonGame/Program.cs
+++ DungeonGame/DungeonGame/Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Threading;
-using Microsoft.VisualBasic;
+using System.Linq;
+
+#pragma warning disable CA1416 // Validate platform compatibility
 
 namespace ConsoleApp
 {
@@ -15,6 +17,13 @@ namespace ConsoleApp
         public string RoomType { get; set; }
         public int Difficulty { get; set; }
         public bool HasEnemy { get; set; }
+
+        public Rooms(string roomType, int difficulty, bool hasEnemy)
+        {
+            RoomType = roomType;
+            Difficulty = difficulty;
+            HasEnemy = hasEnemy;
+        }
     }
 
     public class Enemy // Sets up the basic enemy structure.
@@ -52,11 +61,52 @@ namespace ConsoleApp
         
     }
 
+    public class Player // This is the player class, it will have stats, inventory, etc.
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Gold { get; set; }
+
+        public Player(string name, int level, int health, int attack, int defense, int gold)
+        {
+            Name = name;
+            Level = level;
+            Health = health;
+            Attack = attack;
+            Defense = defense;
+            Gold = gold;
+        }
+    }
+
+    public class Inventory
+    {
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public int ItemQuantity { get; set; }
+
+        public Inventory(string itemName, string itemDescription, int itemQuantity)
+        {
+            ItemName = itemName;
+            ItemDescription = itemDescription;
+            ItemQuantity = itemQuantity;
+        }
+    }
+
     class Game
     {
+        Random rand = new Random();
         public List<Enemy> enemies { get; set; }
+        public List<Rooms> rooms { get; set; }
+        public List<Player> players { get; set; }
         public Game()
         {
+            players = new List<Player>();
+            {
+                players.Add(new Player("", 1, 50, 10, 0, 0));
+            }
             enemies = new List<Enemy>(); // I copied the layout you gave me, while also adding the enemy moves I think.
             {
                 enemies.Add(new Enemy("Slime", 1, 20, "Art here", new List<EnemyMoves>
@@ -76,6 +126,13 @@ namespace ConsoleApp
                     new EnemyMoves("Screech", 0, 50, "Lowered your defense!"),
                     new EnemyMoves("Screech", 5, 75, "Your turn has been skipped!")
                 }));
+            }
+            rooms = new List<Rooms>();
+            {
+                rooms.Add(new Rooms("Empty", 0, false));
+                rooms.Add(new Rooms("Loot", 1, rand.Next(0, 4) == 1));
+                rooms.Add(new Rooms("Trap", 2, rand.Next(0, 6) == 1));
+                rooms.Add(new Rooms("Enemy", 3, true));
             }
         }
         public static void PrintCentered(string text)
@@ -105,24 +162,17 @@ namespace ConsoleApp
                 {
                     case 1:
                         PrintCentered(new string('-', 60));
-                        Console.Beep(100, 800);
                         Thread.Sleep(200);
                         PrintCentered(new string('#', 60));
-                        Console.Beep(200, 800);
                         Thread.Sleep(200);  
-                        PrintCentered(new string('#', 60));
-                        Console.Beep(300, 800);
+                        PrintCentered(new string('@', 60));
                         Thread.Sleep(200);
-                        PrintCentered(new string('#', 60));
-                        Console.Beep(400, 800);
+                        PrintCentered(new string('$', 60));
                         Thread.Sleep(200);
-                        PrintCentered(new string('#', 60));
-                        Console.Beep(480, 800);
+                        PrintCentered(new string('%', 60));
                         Thread.Sleep(200);
                         PrintCentered(new string('-', 60));
-                        Console.Beep(410, 800);
                         PrintCentered("You awake in a cold and dusty dungeon. All you know is you must keep fighting.");
-                        Console.Beep(450, 800);
                         Console.ReadKey();
                         Game game = new Game();
                         Floor1(game);
@@ -149,23 +199,59 @@ namespace ConsoleApp
         /*--- Floors ---*/
         public static void Floor1(Game game)
         {
+            Random rand = new Random();
+            PrintCentered("Please enter your name:");
+            game.players[0].Name = Console.ReadLine();
+            if(string.IsNullOrEmpty(game.players[0].Name))
+            {
+                game.players[0].Name = "Stupid idiot dummy";
+            }
+            PrintCentered($"Welcome, {game.players[0].Name}.");
+            Console.ReadKey();
             Console.Clear();
             PrintCentered("FLOOR ONE");
-            foreach (var enemy in game.enemies) // It simply skips this entire part and asks to continue.
+            Rooms selectedRoom = game.rooms[rand.Next(game.rooms.Count)];
+            foreach (var enemy in game.enemies)
             {
-                PrintCentered($"Enemy: {enemy.Name}, Strength: {enemy.Strength}, Art: {enemy.Art}");
+                PrintCentered($"Enemy: {enemy.Name}\tStrength: {enemy.Strength}\tMoves: {enemy.Moves.First().MoveName}");
             }
-
             PrintCentered("Press any key to continue...");
             Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            PrintCentered(new string('-', 60));
+            Thread.Sleep(250);
+            PrintCentered(new string('@', 60));
+            Thread.Sleep(250);
+            PrintCentered(new string('@', 60));
+            Thread.Sleep(250);
+            PrintCentered("ENCOUNTER ONE...");
+            Thread.Sleep(250);
+            PrintCentered(new string('@', 60));
+            Thread.Sleep(250);
+            PrintCentered(new string('@', 60));
+            Thread.Sleep(250);
+            PrintCentered(new string('-', 60));
+            Thread.Sleep(250);
+            Console.ReadKey();
+            if(selectedRoom.HasEnemy)
+            {
+                PrintCentered($"You entered a {selectedRoom.RoomType} room, and an enemy has appeared!");
+                Console.ReadKey();
+            }
+            else
+            {
+                PrintCentered($"You entered a {selectedRoom.RoomType} room! You're safe, for now...");
+                Console.ReadKey();
+            }
         }
 
         public static void Credits()
         {
             PrintCentered("Credits:");
             Console.WriteLine("Quidd - Programmer");
-            Console.WriteLine("Changelog:");
-            Console.WriteLine("0.0.1 - Initial release");
+            PrintCentered("Changelog:");
+            Console.WriteLine("0.2.0 - added a player class, inventory class, and a basic floor system (03/01/2025)");
+            Console.WriteLine("0.1.0 - Initial release (02/26/2025)");
             Console.WriteLine("Please press any button to return to the main menu.");
             Console.ReadKey();
             Main(null);
---
+++ FakeVirus/FakeVirus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FakeVirus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int amt = 0;
+            while (amt < 10)
+            {
+                Random rand = new Random();
+                int num = rand.Next(1, 3);
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/c dir & timeout /t {num} >nul"; 
+                process.StartInfo.UseShellExecute = true;
+                process.Start();
+
+                amt++;
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string newPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\GhostProgram.exe";
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c timeout /t 2 >nul & move \"{exePath}\" \"{newPath}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+        }
+    }
+}
